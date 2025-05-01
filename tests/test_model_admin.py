555bc93f387bf152656ca1ddcea170b1c88bde0b
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-05-01 18:44:31 +0300

    Write more tests for model admin

--- tests/test_model_admin.py
+++ tests/test_model_admin.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
 from django.core.management import call_command
+from django.core.exceptions import PermissionDenied
 from django.test import RequestFactory, TestCase
 from bs4 import BeautifulSoup
 
@@ -11,6 +12,7 @@ from django_faker_admin.conf import settings
 
 from tests.testapp.models import TestModel
 from tests.testapp.admin import TestModelAdmin
+from tests.testapp.factory import TestModelFactory
 
 
 User = get_user_model()
@@ -46,6 +48,9 @@ class DateHierarchyTests(TestCase):
             *permissions
         )
 
+        cls.size = 5
+        cls.init_data = {'name': 'Test'}
+
     @staticmethod
     def get_permissions_for_model(model):
         ct = ContentType.objects.get_for_model(model)
@@ -63,6 +68,14 @@ class DateHierarchyTests(TestCase):
         )
 
         return permission
+
+    @staticmethod
+    def create_model_instance(**kwargs):
+        instance = TestModelFactory.build(**kwargs)
+        data = instance.__dict__
+        data.pop('_state')
+        data.pop('id')
+        return data
 
     def test_href_exist(self):
         request = self.factory.get(self.list_url)
@@ -97,3 +110,58 @@ class DateHierarchyTests(TestCase):
         self.assertEqual(cl.status_code, 200)
         self.assertFalse(self.orduser.has_perm(self.add_perm_codename))
         self.assertIsNone(add_tag)
+
+    def test_from_get(self):
+        request = self.factory.get(self.dummy_data_url)
+        request.user = self.superuser
+
+        fv = self.model_admin.faker_view(request=request)
+        fv.render()
+
+        soup = BeautifulSoup(fv.content, 'html.parser')
+
+        form_tag = soup.find('form', class_='dummy-data-form')
+        input_tag = form_tag.find('input', {'name': 'size'})
+
+        self.assertEqual(fv.status_code, 200)
+        self.assertTrue(self.superuser.has_perm(self.add_perm_codename))
+        self.assertIsNotNone(form_tag)
+        self.assertIsNotNone(input_tag)
+        self.assertEqual(input_tag['name'], 'size')
+
+    def test_form_submit(self):
+        data = self.create_model_instance(**self.init_data)
+        data['size'] = self.size
+
+        request = self.factory.post(self.dummy_data_url, data=data)
+        request.user = self.superuser
+
+        fv = self.model_admin.faker_view(request=request)
+
+        qs = TestModel.objects.filter(
+            ** self.init_data
+        )
+
+        self.assertEqual(fv.status_code, 302)
+        self.assertTrue(self.superuser.has_perm(self.add_perm_codename))
+        self.assertEqual(self.size, qs.count())
+
+    def test_form_submit_no_perm(self):
+        permission = self.get_permission_from_codename(self.add_perm_codename)
+        self.orduser.user_permissions.remove(permission)
+
+        data = self.create_model_instance(**self.init_data)
+        data['size'] = self.size
+
+        request = self.factory.post(self.dummy_data_url, data=data)
+        request.user = self.orduser
+
+        with self.assertRaises(PermissionDenied):
+            self.model_admin.faker_view(request=request)
+
+        qs = TestModel.objects.filter(
+            ** self.init_data
+        )
+
+        self.assertFalse(self.orduser.has_perm(self.add_perm_codename))
+        self.assertEqual(0, qs.count())
