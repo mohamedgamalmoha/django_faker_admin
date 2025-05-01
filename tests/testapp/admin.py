d32e8f68f0d19a3ebd76173f5e7a21392ba2d0de
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-05-01 18:11:42 +0300

    Add factory class to model admin

--- tests/testapp/admin.py
+++ tests/testapp/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django_faker_admin import FakerModelAdminMixin
 
 from .models import TestModel
+from .factory import TestModelFactory
 
 
 @admin.register(TestModel)
 class TestModelAdmin(FakerModelAdminMixin, admin.ModelAdmin):
+    factory_class = TestModelFactory
     list_display = ('id', 'name', 'description')
     search_fields = ('name', 'description')
