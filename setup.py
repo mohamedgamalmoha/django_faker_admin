4648bbeec7a39caa461a14277c9d1fa97c72966c
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-06-29 15:44:41 +0300

    Add configs to setup function

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 if __name__ == "__main__":
-    setup()
+    setup(
+        package_dir={"": "src"},
+        packages=find_packages(where="src"),
+        include_package_data=False,
+        package_data={
+            "django_faker_admin": [
+                "templates/**/*.html",
+            ],
+        }
+    )
